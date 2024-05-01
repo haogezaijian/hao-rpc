5e2da786eef228ce241d1d0e5fbcb54ad9a4f612
haoge <1668173696@qq.com>
2024-05-01 15:02:40 +0800

    通用模块

---
+++ example-common/src/main/java/com/hao/example/common/service/UserService.java
@@ -0,0 +1,16 @@
+package com.hao.example.common.service;
+
+import com.hao.example.common.model.User;
+
+public interface UserService {
+    /**
+     * get user
+     *
+     * @param user user
+     * @return {@code User}
+     * @author jiangyihao
+     * @version 5.0.0
+     * @date 2024/05/01
+     */
+    User getUser(User user);
+}
